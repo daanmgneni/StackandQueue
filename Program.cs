f3abb940dfd8ae51c6b3fac7c8f1c32fe7a47974
DAAN <daanmgneni@gmail.com>
2023-04-01 04:19:28 +0530

    Created stack using Linked list

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace day14_StackAndQueuesProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Queue Program!");
+            Console.WriteLine("#####################");
+
+            StackWithLinkedList obj = new StackWithLinkedList();
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            obj.Display();
+        }
+
+    }
+}
+
